39d08c646dabcc81dd4e5a8499c7360eda747a07
李菲 <lifei3599@126.com>
2017-03-21 13:17:28 +0800

    舆情监测头部搜索框

--- edu/src/views/pomp/analyse/analyse.vue
+++ edu/src/views/pomp/analyse/analyse.vue
@@ -2,30 +2,45 @@
 * Created by zhangxin on 2017/3/16.
 */
 <template>
-    <div>
-        {{msg}}
+    <div class="analyse">
+        <div class="navbar">
+            <el-breadcrumb separator=">">
+                <el-breadcrumb-item>舆情管理</el-breadcrumb-item>
+                <el-breadcrumb-item>舆情监测</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div id="search-container">
+            <search-box></search-box>
+        </div>
+        <div class="content">
+            <div class="content-bar">
+
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
+
 </style>
 <script>
-     /*
-    * import '../../assets/vendor/iCkeck-v1.0.2/js/icheck.min';
-    * import "vue-style-loader!css-loader!sass-loader!../../assets/vendor/iCkeck-v1.0.2/css/skins/square/blue.css";
-    * import loginButton from './components/loginButton.vue';
-    */
+    /*
+     * import '../../assets/vendor/iCkeck-v1.0.2/js/icheck.min';
+     * import "vue-style-loader!css-loader!sass-loader!../../assets/vendor/iCkeck-v1.0.2/css/skins/square/blue.css";
+     * import loginButton from './components/loginButton.vue';
+     */
+    import "vue-style-loader!css-loader!sass-loader!./css/analyse.scss";
+    import searchBox from '../../../components/searchBox/searchBox.vue'
+
     export default{
         data(){
             return {
-                msg:"舆情监测"
+                msg: "舆情监测"
             }
         },
-        components:{} ,
-        methods:{
-            
-        },
+        components: {searchBox},
+        methods: {},
         mounted(){
-            
+
         }
     }
 </script>
