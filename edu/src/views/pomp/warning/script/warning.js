5b7d5520dc730d858c9e10cf5c53259a2c23f3ac
张鑫 <zhangxin11032@vip.qq.com>
2017-03-21 13:48:06 +0800

    动态面包屑，结构布局修改。

---
+++ edu/src/views/pomp/warning/script/warning.js
@@ -0,0 +1,37 @@
+/**
+ * Created by zhangxin on 2017/3/21.
+ */
+/*
+ * import '../../assets/vendor/iCkeck-v1.0.2/js/icheck.min';
+ * import "vue-style-loader!css-loader!sass-loader!../../assets/vendor/iCkeck-v1.0.2/css/skins/square/blue.css";
+ * import loginButton from './components/loginButton.vue';
+ */
+import breadCrumb from '../../../../components/breadCrumb/breadCrumb.vue';
+export default{
+    data(){
+        return {
+            msg:"舆情预警"
+        }
+    },
+    components:{breadCrumb} ,
+    methods:{
+        setBreadCrumb(){
+            let breadcrumb=[
+                {
+                    name:"舆情管理",to:{path:"/"}
+                },
+                {
+                    name:"舆情预警",to:{path:"/warning"}
+                }
+            ];
+            this.$store.commit("setBreadCrumb",breadcrumb);
+        }
+    },
+    created(){
+        this.setBreadCrumb();
+    },
+    mounted(){
+        /*设置面包屑*/
+
+    }
+}
