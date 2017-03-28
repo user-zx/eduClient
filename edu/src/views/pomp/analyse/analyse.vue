7ffd30d726bf3f700dc9ec171a858282944fb442
weiyuc <yuwei_149@163.com>
2017-03-28 16:43:09 +0800

    添加筛选交互实现

--- edu/src/views/pomp/analyse/analyse.vue
+++ edu/src/views/pomp/analyse/analyse.vue
@@ -5,7 +5,7 @@
     <div class="analyse article-wrap">
         <bread-crumb></bread-crumb>
         <div id="search-container">
-            <search-box :searchData=searchData></search-box>
+            <search-box :searchNames=searchNames @searchDataChange="onSearchDataChange"></search-box>
         </div>
         <div class="content">
             <div class="content-bar">
@@ -64,13 +64,13 @@
         data(){
             return {
                 msg: "舆情监测",
-                searchData:"",
                 currentPage: 1,
                 currentTabs: {
                     articleView: 'articleView',
                     characterTable: 'characterTable',
                     currentTab: 'articleView'
-                }
+                },
+                searchNames: ['university', 'dimension', 'vector', 'emotion', 'publishDateTime']
             }
         },
         components: {breadCrumb, searchBox, articleView, characterTable},
@@ -94,102 +94,16 @@
                 this.currentPage = val;
                 console.log(`当前页: ${val}`);
             },
-
+            onSearchDataChange(data) {
+                console.log("change callback, data :");
+                console.log(data);
+            }
 
         },
         created(){
             this.setBreadCrumb();
         },
         mounted(){
-            let searchData = [
-                {
-                    'title' :  '选择高校:',
-                    'searchList': [
-                        {id: 0, text: '全部', selected: true},
-                        {id: 1, text: '北京大学', selected: false},
-                        {id: 2, text: '北京航空航天大学', selected: false},
-                        {id: 3, text: '清华大学', selected: false},
-                        {id: 4, text: '中央民族大学', selected: false},
-                        {id: 5, text: '北京邮电大学', selected: false},
-                        {id: 6, text: '北京传媒大学', selected: false},
-                        {id: 7, text: '北京科技大学', selected: false},
-                        {id: 8, text: '北京科技大学', selected: false},
-                        {id: 9, text: '北京科技大学', selected: false},
-                        {id: 10, text: '北京科技大学', selected: false},
-                        {id: 11, text: '北京科技大学', selected: false}
-                    ],
-                    'hasMore': true,
-                    'multiple': false,
-                    'buttons': [
-                        {'buttonText': '添加高校', 'id': 'add-college-btn'}
-                    ],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '高校维度:',
-                    'searchList': [
-                        {id: 0, text: '校园舆情', selected: true, showCharacter: false},
-                        {id: 1, text: '校园安全', selected: false, showCharacter: false},
-                        {id: 2, text: '违规违纪', selected: false, showCharacter: false},
-                        {id: 3, text: '媒体报道', selected: false, showCharacter: false},
-                        {id: 4, text: '人才培养', selected: false, showCharacter: false},
-                        {id: 5, text: '科学研究', selected: false, showCharacter: false},
-                        {id: 6, text: '微信微博', selected: false, showCharacter: false},
-                        {id: 7, text: '论坛贴吧', selected: false, showCharacter: false},
-                        {id: 8, text: '敏感时期', selected: false, showCharacter: false},
-                        {id: 9, text: '人物聚焦', selected: false, showCharacter: true}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '载体:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: true},
-                        {id: 1, text: '论坛', selected: false},
-                        {id: 2, text: '微博', selected: false},
-                        {id: 3, text: '微信', selected: false},
-                        {id: 4, text: 'QQ群', selected: false},
-                        {id: 5, text: '博客', selected: false},
-                        {id: 6, text: '网站门户', selected: false},
-                        {id: 7, text: '贴吧', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '情感:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: false},
-                        {id: 1, text: '正面', selected: true},
-                        {id: 2, text: '负面', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '时间:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: false},
-                        {id: 1, text: '今天', selected: true},
-                        {id: 2, text: '昨天', selected: false},
-                        {id: 3, text: '近7天', selected: false},
-                        {id: 4, text: '近一个月', selected: false},
-                        {id: 5, text: '自定义时间', dateBox: 'show', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': true
-                }
-            ];
-            this.searchData = searchData;
         },
     }
 </script>
