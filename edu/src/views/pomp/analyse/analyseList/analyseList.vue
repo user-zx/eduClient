2e0e42b554890d76f4c1d1149b3716fa6eb35b98
weiyuc <yuwei_149@163.com>
2017-03-29 20:03:36 +0800

    修改预警监测页面

--- edu/src/views/pomp/analyse/analyseList/analyseList.vue
+++ edu/src/views/pomp/analyse/analyseList/analyseList.vue
@@ -2,16 +2,18 @@
 * Created by lifei on 2017/3/28.
 */
 <template>
-    <div>
+    <div v-loading="loading" element-loading-text="加载中……">
         <search-box :searchNames=searchNames @searchDataChange="onSearchDataChange"></search-box>
         <div class="content">
             <div class="content-bar">
                 <ul class="content-bar-list">
                     <li class="pointer">全部</li>
-                    <li class="pointer arrow-up">
-                        阅读量
+                    <li class="pointer" @click="sort(0)">
+                        阅读量<i class="arrow" :class="param.orders[0].direction == 'DESC' ? 'arrow-up' : 'arrow-down'"></i>
                     </li>
-                    <li class="pointer arrow-down">时间</li>
+                    <li class="pointer" @click="sort(1)">
+                        时间<i class="arrow" :class="param.orders[1].direction == 'DESC' ? 'arrow-up' : 'arrow-down'"></i>
+                    </li>
                 </ul>
                 <div class="content-bar-button">
                     <el-dropdown class="event-store-box" trigger="click">
@@ -29,16 +31,15 @@
                 </div>
                 <div class="content-bar-page">
                     <el-pagination class="edu-pagination"
-                                   @size-change="handleSizeChange"
                                    @current-change="handleCurrentChange"
                                    :current-page="currentPage"
                                    :page-size="5"
                                    layout="prev, next, jumper, total"
-                                   :total="100">
+                                   :total="total">
                     </el-pagination>
                 </div>
             </div>
-            <components :is="currentTabs.currentTab"></components>
+            <components :is="currentTabs.currentTab" :articleData="articleData"></components>
         </div>
     </div>
 </template>
@@ -50,14 +51,31 @@
         data(){
             return {
                 msg: "舆情监测",
-                searchData:"",
                 currentPage: 1,
+                total: 0,
                 currentTabs: {
                     articleView: 'articleView',
                     characterTable: 'characterTable',
                     currentTab: 'articleView'
                 },
-                searchNames: ['university', 'dimension', 'vector', 'emotion', 'publishDateTime']
+                param: {
+                    pageSize: 5,
+                    pageNumber: 0,
+                    dimension: '校园舆情',
+                    orders: [
+                        {
+                            property: 'hitCount',
+                            direction: 'DESC'
+                        },
+                        {
+                            property: 'publishDateTime',
+                            direction: 'DESC'
+                        }
+                    ]
+                },
+                searchNames: ['university', 'dimension', 'vector', 'emotion', 'publishDateTime'],
+                articleData: [],
+                loading:true
             }
         },
         components: {searchBox, articleView, characterTable},
@@ -73,17 +91,44 @@
                 ];
                 this.$store.commit("setBreadCrumb",breadcrumb);
             },
-
-            handleSizeChange(val) {
-                console.log(`每页 ${val} 条`);
-            },
-            handleCurrentChange(val) {
-                this.currentPage = val;
-                console.log(`当前页: ${val}`);
+            handleCurrentChange(pageNumber) {
+                //后台是从0开始
+                this.param.pageNumber = pageNumber - 1;
+                this.getArticleList();
             },
             onSearchDataChange(data) {
-                console.log("change callback, data :");
-                console.log(data);
+                data.pageSize = 5;
+                data.pageNumber = 0;
+                data.orders = this.param.orders;
+                this.param = data;
+                this.currentPage = 1;
+                this.getArticleList();
+            },
+            sort(index) {
+                this.param.orders[index].direction = this.param.orders[index].direction == 'DESC' ? 'ASC' : 'DESC';
+                this.getArticleList();
+            },
+            getArticleList() {
+                this.loading = true;
+                this.$nextTick(function() {
+                    this.$http.post('/apis/opinionMonitor/getOpinionMonitor.json', this.param).then(
+                        (response) => {
+                            if (response.data.success) {
+                                this.articleData = response.data.data.content;
+                                // 最多允许翻1000页
+                                this.total = response.data.data.totalElements > 10000 ? 10000 : response.data.data.totalElements;
+                                this.$nextTick(function() {
+                                    this.loading = false;
+                                });
+                            } else {
+                                console.error(response.data.message);
+                            }
+                        }, (response) => {
+                            console.error(response);
+                            this.loading = false;
+                        }
+                    );
+                });
             }
 
         },
@@ -91,95 +136,7 @@
             this.setBreadCrumb();
         },
         mounted(){
-            let searchData = [
-                {
-                    'title' :  '选择高校:',
-                    'searchList': [
-                        {id: 0, text: '全部', selected: true},
-                        {id: 1, text: '北京大学', selected: false},
-                        {id: 2, text: '北京航空航天大学', selected: false},
-                        {id: 3, text: '清华大学', selected: false},
-                        {id: 4, text: '中央民族大学', selected: false},
-                        {id: 5, text: '北京邮电大学', selected: false},
-                        {id: 6, text: '北京传媒大学', selected: false},
-                        {id: 7, text: '北京科技大学', selected: false},
-                        {id: 8, text: '北京科技大学', selected: false},
-                        {id: 9, text: '北京科技大学', selected: false},
-                        {id: 10, text: '北京科技大学', selected: false},
-                        {id: 11, text: '北京科技大学', selected: false}
-                    ],
-                    'hasMore': true,
-                    'multiple': false,
-                    'buttons': [
-                        {'buttonText': '添加高校', 'id': 'add-college-btn'}
-                    ],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '高校维度:',
-                    'searchList': [
-                        {id: 0, text: '校园舆情', selected: true, showCharacter: false},
-                        {id: 1, text: '校园安全', selected: false, showCharacter: false},
-                        {id: 2, text: '违规违纪', selected: false, showCharacter: false},
-                        {id: 3, text: '媒体报道', selected: false, showCharacter: false},
-                        {id: 4, text: '人才培养', selected: false, showCharacter: false},
-                        {id: 5, text: '科学研究', selected: false, showCharacter: false},
-                        {id: 6, text: '微信微博', selected: false, showCharacter: false},
-                        {id: 7, text: '论坛贴吧', selected: false, showCharacter: false},
-                        {id: 8, text: '敏感时期', selected: false, showCharacter: false},
-                        {id: 9, text: '人物聚焦', selected: false, showCharacter: true}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '载体:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: true},
-                        {id: 1, text: '论坛', selected: false},
-                        {id: 2, text: '微博', selected: false},
-                        {id: 3, text: '微信', selected: false},
-                        {id: 4, text: 'QQ群', selected: false},
-                        {id: 5, text: '博客', selected: false},
-                        {id: 6, text: '网站门户', selected: false},
-                        {id: 7, text: '贴吧', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '情感:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: false},
-                        {id: 1, text: '正面', selected: true},
-                        {id: 2, text: '负面', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': false
-                },
-                {
-                    'title': '时间:',
-                    'searchList': [
-                        {id: 0, text: '不限', selected: false},
-                        {id: 1, text: '今天', selected: true},
-                        {id: 2, text: '昨天', selected: false},
-                        {id: 3, text: '近7天', selected: false},
-                        {id: 4, text: '近一个月', selected: false},
-                        {id: 5, text: '自定义时间', dateBox: 'show', selected: false}
-                    ],
-                    'hasMore': false,
-                    'multiple': false,
-                    'buttons': [],
-                    'hasDateBox': true
-                }
-            ];
-            this.searchData = searchData;
+            this.getArticleList();
         },
     }
 </script>
