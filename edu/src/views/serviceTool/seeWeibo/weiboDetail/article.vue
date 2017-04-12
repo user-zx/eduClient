9ba40d69440d77903f53ac49553700e3cdb56438
李菲 <lifei3599@126.com>
2017-04-12 21:46:46 +0800

    ...........

---
+++ edu/src/views/serviceTool/seeWeibo/weiboDetail/article.vue
@@ -0,0 +1,203 @@
+/**
+* Created by lifei on 2017/3/21.
+*/
+
+<template>
+    <div class="article-container" id="articleContainer" :data="articleData">
+        <div class="article" v-for="(item, index) in articleData">
+            <div class="article-left">
+                <div class="img-div">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div class="article-right">
+                <div class="article-title-box">
+                    <p class="article-title">
+                        <span>{{item.title}}</span>
+                        <i class="title-icon positive-icon" v-if="item.emotion == 'positive'"></i>
+                        <i class="title-icon negative-icon" v-else></i>
+                    </p>
+                    <p class="button-box">
+                        <el-button type="share" class="article-share-button"
+                                   @click="shareBtnClick(item)" :loading="item.loading">
+                            分享
+                        </el-button>
+                    </p>
+                </div>
+                <p class="article-main">
+                    {{item.content}}
+                </p>
+                <p class="article-describe">
+                    <span class="article-source">
+                        来源： {{item.source}}
+                    </span>
+                    <span class="article-author">
+                        作者： {{item.author}}
+                    </span>
+                    <span class="article-clickNum blue">
+                        阅读量： {{item.hitCount}}
+                    </span>
+                    <span class="article-publishDate blue">
+                        发布时间：{{item.publishDateTime}}
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .article-container{
+        background: #ffffff;
+        .article{
+            border-bottom: 1px solid #E3EDFB;
+            display: table;
+
+            .article-left{
+                width: 7%;
+                height: 100%;
+                display: table-cell;
+                text-align: center;
+                vertical-align: middle;
+                border-right: 1px solid #E3EDFB;
+            }
+
+            .article-right{
+                height: inherit;
+                width:92%;
+                display: table-cell;
+                padding: 30px 30px 30px 12px;
+                color: #6a6a6a;
+                vertical-align: top;
+
+                .article-title-box{
+                    height: 28px;
+
+                    .article-title{
+                        font-weight: normal;
+                        font-size: 20px;
+                        width: 79%;
+                        height: 100%;
+                        display: inline-block;
+                        position: relative;
+
+                        span:first-child{
+                            display: inline-block;
+                            height:100%;
+                            max-width: 680px;
+                            overflow: hidden;
+                            margin-right: 10px;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                        }
+                        .title-icon{
+                            position: absolute;
+                            display: inline-block;
+                            width: 46px;
+                            font-size: 14px;
+                            text-align: center;
+                            border-radius: 3px;
+                            top: 3px;
+                        }
+
+                        .positive-icon{
+                            color: #22ac38;
+                            border: 1px solid #22ac38;
+                        }
+
+                        .positive-icon::before{
+                            content: '正面';
+                        }
+
+                        .negative-icon{
+                            color: #e60012;
+                            border: 1px solid #e60012;
+                        }
+
+                        .negative-icon::before{
+                            content: '负面';
+                        }
+                    }
+                    .button-box{
+                        width: 20%;
+                        display: inline-block;
+                    }
+                }
+
+                .article-describe{
+                    font-size: 12px;
+                    margin-top: 13px;
+                    text-align: right;
+
+                    .article-source{
+                        margin-right: 14px;
+                    }
+                    .article-author, .article-clickNum{
+                        margin-right: 16px;
+                    }
+
+                    article-publishDate{
+                        line-height: 20px;
+                    }
+
+                    .blue{
+                        color: #85b8ff;
+                        font-size:14px;
+                    }
+                }
+
+                .article-main{
+                    line-height: 24px;
+                    margin-top:13px;
+                }
+            }
+
+            .article-share-button{
+                float: right;
+                margin-top: -8px;
+                transition: width 0.5s;
+                -moz-transition: width 0.5s; /* Firefox 4 */
+                -webkit-transition:width 0.5s; /* Safari 和 Chrome */
+                -o-transition: width 0.5s; /* Opera */
+            }
+        }
+        &.dark{
+             background-color: #21273d;
+            .article{
+                border-bottom-color: rgba(96,163,255,.1);
+            }
+            .article-left{
+                border-right-color:rgba(96,163,255,.1);
+            }
+            .article-right{
+                color:#d0d7ff;
+
+                .article-title-box{
+                    .article-title{
+                        color:#60a3ff;
+                    }
+                }
+            }
+        }
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return {
+                msg: ""
+            }
+        },
+        components: {},
+        methods: {
+            shareBtnClick(item){
+                item.loading = true;
+                console.log(item);
+                this.$nextTick(function () {
+
+                });
+            }
+        },
+        props: ["articleData"]
+    }
+</script>
