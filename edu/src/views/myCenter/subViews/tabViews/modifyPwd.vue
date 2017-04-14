63d41363d500556b5e2a638ab7e9e80f9a923976
李菲 <lifei3599@126.com>
2017-04-14 09:33:00 +0800

    密码修改-

--- edu/src/views/myCenter/subViews/tabViews/modifyPwd.vue
+++ edu/src/views/myCenter/subViews/tabViews/modifyPwd.vue
@@ -4,15 +4,15 @@
 <template>
     <div class="modifyPwd">
        <div class="form-wrap">
-           <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="pwdForm dark">
-               <el-form-item prop="curPwd">
-                   <el-input type="password" v-model="ruleForm2.curPwd" placeholder="输入当前密码" auto-complete="off"></el-input>
+           <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="pwdForm dark" id="pwdForm">
+               <el-form-item prop="oldPwd">
+                   <el-input type="password" v-model="ruleForm2.oldPwd" placeholder="输入当前密码" auto-complete="off" name="oldPwd"></el-input>
                </el-form-item>
                <el-form-item label="" prop="pass">
-                   <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="输入新密码"></el-input>
+                   <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="输入新密码" name="newPwd1"></el-input>
                </el-form-item>
                <el-form-item prop="checkPass">
-                   <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="确认新密码"></el-input>
+                   <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="确认新密码" name="newPwd2"></el-input>
                </el-form-item>
 
                <el-form-item>
@@ -40,7 +40,7 @@
 <script>
     export default{
         data(){
-            var checkCurPwd = (rule, value, callback) => {
+            var checkOldPwd = (rule, value, callback) => {
                 //验证当前密码是否正确
                 if(value === ''){
                     callback(new Error('请输入当前密码'));
@@ -48,6 +48,18 @@
                     callback();
                 }
             };
+            var validNewOldPass = (rule, value, callback) =>{
+                    if(value == ''){
+                        callback(new Error('请输入密码'));
+                    }else {
+                        if(this.ruleForm2.oldPwd != '' && this.ruleForm2.oldPwd == value){
+                            callback(new Error('新密码不能与旧密码相同!'));
+                        }else {
+                            callback();
+                        }
+                    }
+            };
+
             var validatePass = (rule, value, callback) => {
                 if (value === '') {
                     callback(new Error('请输入密码'));
@@ -71,17 +83,20 @@
                 ruleForm2: {
                     pass: '',
                     checkPass: '',
-                    curPwd: ''
+                    oldPwd: ''
                 },
                 rules2: {
                     pass: [
+                        {validator: validNewOldPass, trigger: 'blur'},
                         { validator: validatePass, trigger: 'blur' }
                     ],
                     checkPass: [
-                        { validator: validatePass2, trigger: 'blur' }
+                        { validator: validatePass2, trigger: 'blur' },
+                        { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
                     ],
-                    curPwd: [
-                        { validator: checkCurPwd, trigger: 'blur' }
+                    oldPwd: [
+                        { validator: checkOldPwd, trigger: 'blur' },
+                        { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
                     ]
                 }
             };
@@ -99,8 +114,62 @@
                 this.$store.commit("setBreadCrumb",breadcrumb);
             },
 
-            submitForm(data){
-                console.log(data)
+            submitForm(formName){
+                console.log(formName)
+                this.$refs[formName].validate(
+                    (valid) => {
+                        if(valid){
+                            //
+                            this.$http.post('/apis/security/generateKey.do').then(
+                                (response) => {
+                                    if (response.data.success) {
+                                        let exponent = response.data.data.publicKeyExponent;
+                                        let modulus = response.data.data.publicKeyModulus;
+                                        RSAUtils.setMaxDigits(200);
+                                        let key = new RSAUtils.getKeyPair(exponent, "", modulus);
+                                        //新密码
+                                        let password =  this.ruleForm2.pass;
+                                        //旧密码
+                                        let oldPwd = this.ruleForm2.oldPwd;
+                                        let encrypedNewPwd = RSAUtils.encryptedString(key,password);
+                                        let encrypedOldPwd = RSAUtils.encryptedString(key,oldPwd);
+                                        $('input[name=oldPwd]').val(encrypedOldPwd);
+                                        $('input[name=newPwd1]').val(encrypedNewPwd);
+                                        $('input[name=newPwd2]').val(encrypedNewPwd);
+                                        this.ruleForm2.oldPwd = encrypedOldPwd;
+                                        this.ruleForm2.pass = encrypedNewPwd;
+                                        this.ruleForm2.checkPass = encrypedNewPwd;
+                                        this.updatePwd();
+                                    } else {
+                                        console.error(response.data.msg);
+                                        return false;
+                                    }
+                                }
+                            )
+                        }else{
+                            //表单填写是否完全
+
+                        }
+                    }
+                )
+            },
+
+            updatePwd(){
+                let param = {
+                    oldPassword: this.ruleForm2.oldPwd,
+                    newPassword: this.ruleForm2.pass
+                }
+                console.log(param)
+                this.$http.post('/apis/user/updatePassword.json', param).then(
+                    (response) => {
+                        console.log(response.data)
+                        if(response.data.success){
+
+                        }else {
+
+                        }
+                    }
+                )
             }
         },
         mounted(){
