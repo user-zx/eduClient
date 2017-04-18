c76d563584bcb0a5e5a33abf837fd423afc3fc20
余伟 <yuwei@ACA81E59.ipt.aol.com>
2017-04-18 20:38:18 +0800

    添加申请实现

--- edu/src/views/login/probation/probation.vue
+++ edu/src/views/login/probation/probation.vue
@@ -6,38 +6,37 @@
         <h1 class="login-title">
             <span>申请试用</span>
         </h1>
-        <el-form :model="form" action="/apis/login.do" method="post" :rules="rules" ref="ruleForm" label-width="0" class="ruleForm dark">
-            <el-form-item label="">
-                <el-input v-model="form.name" placeholder="请输入您的称呼"></el-input>
+        <el-form :model="form" method="post" :rules="rules" ref="ruleForm" label-width="0" class="ruleForm dark">
+            <el-form-item label="" prop="realName">
+                <el-input v-model="form.realName" placeholder="请输入您的称呼"></el-input>
             </el-form-item>
-            <el-form-item label="">
-                <el-input v-model="form.schoolName" placeholder="请输入您所在高校全称"></el-input>
+            <el-form-item label="" prop="collegeName">
+                <el-input v-model="form.collegeName" placeholder="请输入您所在高校全称"></el-input>
             </el-form-item>
-            <el-form-item label="">
-                <el-input v-model="form.professor" placeholder="请输入您的职称"></el-input>
+            <el-form-item label="" prop="userDepartment">
+                <el-input v-model="form.userDepartment" placeholder="请输入您的职称"></el-input>
             </el-form-item>
-            <el-form-item label="">
-                <el-input v-model="form.phone" placeholder="请输入手机号"></el-input>
+            <el-form-item label="" prop="userPhone">
+                <el-input v-model="form.userPhone" placeholder="请输入手机号"></el-input>
             </el-form-item>
-            <el-form-item label="">
+            <el-form-item label="" prop="captcha">
                 <el-col :span="11">
-                    <el-input v-model="form.identifyingCode" placeholder="请输入验证码"></el-input>
+                    <el-input v-model="form.captcha" placeholder="请输入验证码"></el-input>
                 </el-col>
                 <el-col :span="2">
-                    &nbsp;
                 </el-col>
                 <el-col :span="11" class="text-center">
-                    <el-button type="primary submit">获取验证码</el-button>
+                    <img src="/apis/security/captcha.do" ID="captcha" onclick="this.src = this.src + '?' + Math.random()"/>
                 </el-col>
             </el-form-item>
-            <el-form-item label="">
-                <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+            <el-form-item label="" prop="userEmail">
+                <el-input v-model="form.userEmail" placeholder="请输入邮箱"></el-input>
+            </el-form-item>
+            <el-form-item label="" prop="area">
+                <el-input v-model="form.area" placeholder="请输入您的所在地（省／市／县）"></el-input>
             </el-form-item>
             <el-form-item label="">
-                <el-input v-model="form.site" placeholder="请输入您的所在地（省／市／县）"></el-input>
-            </el-form-item>
-            <el-form-item label="">
-                <el-button type="primary submit" @click="onSubmit">提交申请</el-button>
+                <el-button type="primary submit" @click="submitForm('ruleForm')">提交申请</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -53,24 +52,80 @@
             return {
                 msg:"申请试用",
                 form: {
-                    name: '',
-                    schoolName: '',
-                    professor: '',
-                    phone: '',
-                    identifyingCode: '',
-                    email: '',
-                    site: '',
+                    realName: '',
+                    collegeName: '',
+                    userDepartment: '',
+                    userPhone: '',
+                    userEmail: '',
+                    area: '',
+                    captcha: '',
                 },
                 rules:{
-
+                    realName:[
+                        {required:true, message:"请输入称呼", trigger: 'blur' },
+                        {min:2,max:10,message: "长度在 2 到 10 个字符",trigger: 'blur' },
+                    ],
+                    collegeName:[
+                        {required:true, message:"请输入高校全称", trigger: 'blur' },
+                        {min:4,max:20,message: "长度在 4 到 20 个字符",trigger: 'blur' },
+                    ],
+                    userDepartment:[
+                        {required:true, message:"请输入职称", trigger: 'blur' },
+                        {min:2,max:10,message: "长度在 2 到 10 个字符",trigger: 'blur' },
+                    ],
+                    userPhone:[
+                        {required:true,message:"请输入手机号",trigger: 'blur' },
+                        {min:11, max:11, message: "请输入正确手机号",trigger: 'blur' }
+                    ],
+                    userEmail:[
+                        {required:true,message:"请输入邮箱",trigger: 'blur' },
+                        {type: 'email', message: "请输入正确的邮箱",trigger: 'blur' }
+                    ],
+                    area:[
+                        {required:true, message:"请输入地区", trigger: 'blur' },
+                        {min:2, max:10, message: "长度在 2 到 10 个字符", trigger: 'blur' },
+                    ],
+                    captcha:[
+                        {required:true, message:"请输入验证码", trigger: 'blur' },
+                        {min:4,max:4,message: "长度在 4 个字符",trigger: 'blur' },
+                    ]
                 }
             }
         },
         components:{} ,
         methods:{
-            onSubmit() {
-                console.log('submit!');
-            }
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.$http.post('/apis/addTrial.json', this.form).then((response) => {
+                                if (response.data.success) {
+                                    this.$message({
+                                        message: '申请成功',
+                                        type: 'success'
+                                    });
+                                    this.resetForm(formName);
+                                } else {
+                                    this.$message({
+                                        message: response.data.message,
+                                        type: 'error'
+                                    });
+                                    $('#captcha').attr('src', $('#captcha').attr('src') + '?' + Math.random());
+                                }
+                            }, (response) => {
+                                console.error(response);
+                                return false;
+                            }
+                        );
+                    } else {
+                        this.$message({message:'校验失败!',type:"error"});
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                /*重置表单*/
+                this.$refs[formName].resetFields();
+            },
         },
         mounted(){
             
